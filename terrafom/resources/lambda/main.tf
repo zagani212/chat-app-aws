084f6feb929d8cde2987f3e649346b3f5bf61c3c
zagani212 <abdelhakassahi@gmail.com>
2026-04-16 01:16:11 +0200

    add lmabda function module

---
+++ terrafom/resources/lambda/main.tf
@@ -0,0 +1,38 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = var.source_file
+  output_path = format("./zip/%s.zip", var.function_name)
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = var.function_name
+
+  runtime = "nodejs18.x"
+  handler = var.handler
+  layers = [var.layer_arn]
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
---
+++ terrafom/resources/lambda/variables.tf
@@ -0,0 +1,16 @@
+variable source_file {
+  type = string
+  description = "The source file of your function"
+}
+variable function_name {
+  type = string
+  description = "The name of your function"
+}
+variable layer_arn {
+  type = string
+  description = "The arn of the layer used by your function"
+}
+variable handler {
+  type = string
+  description = "The handler of the function"
+}
