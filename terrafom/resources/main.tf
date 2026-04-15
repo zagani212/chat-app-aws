9af0b2e03e086dc685ae127f347963168c78af52
zagani212 <abdelhakassahi@gmail.com>
2026-04-16 01:17:30 +0200

    create first function using a layer

--- terrafom/resources/main.tf
+++ terrafom/resources/main.tf
@@ -7,3 +7,20 @@ terraform {
     encrypt = true
   }
 }
+
+module "lambda_layer" {
+  source = "./lambdaLayer"
+
+  source_file = "../../backend/layer/nodejs/PostToConnection.mjs"
+  layer_name = "postToConnection"
+  runtimes = ["nodejs20.x"]
+}
+
+module "lambda_function" {
+  source = "./lambda"
+
+  source_file = "../../backend/GetRooms.mjs"
+  function_name = "GetRooms"
+  layer_arn = module.lambda_layer.layer_arn
+  handler = "GetRooms.handler"
+}
