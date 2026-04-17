3bcabf4cca4e08ed7d20611303d04b41c79eedef
zagani212 <abdelhakassahi@gmail.com>
2026-04-17 13:41:48 +0200

    add variables to policies module

---
+++ terrafom/resources/policies/variables.tf
@@ -0,0 +1,20 @@
+variable "chat_room_table" {
+  type = string
+  description = "The ARN of chat room table"
+}
+variable "connection_table" {
+  type = string
+  description = "The ARN of connection table"
+}
+variable "message_table" {
+  type = string
+  description = "The ARN of message table"
+}
+variable "user_table" {
+  type = string
+  description = "The ARN of user table"
+}
+variable "user_room_table" {
+  type = string
+  description = "The ARN of user room table"
+}
