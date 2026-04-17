af011a908e09c282ac57737443f0778f65ea4bec
zagani212 <abdelhakassahi@gmail.com>
2026-04-17 13:43:43 +0200

    bootsrap module

--- terrafom/bootstrap/main.tf
+++ terrafom/bootstrap/main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encrypti
 resource "aws_dynamodb_table" "tf_lock" {
   name         = "terraform-state-lock"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  key_schema     = "LockID"
 
   attribute {
     name = "LockID"
